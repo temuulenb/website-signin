ea09777f36622ee30459dcd403ff46e44765a50d
TemuulenBayasgalan <temuu1109@gmail.com>
2023-07-07 17:45:02 -0700

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 
 function App() {
   return (
     <div>
-        <h1>Hello world!</h1>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+                <Route path="/sign-in" element={<SignIn />}></Route>
+                <Route path="/sign-up" element={<SignUp />}></Route> 
+                <Route path="/forgot-password" element={<ForgotPassword />}></Route>  
+            </Routes>
+        </Router>
     </div>
   );
 }
