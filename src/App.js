629f008ce8143e3f444907546c126e7e947fc615
TemuulenBayasgalan <temuu1109@gmail.com>
2023-07-10 16:55:47 -0700

    finished log out step

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Main from "./pages/Main";
+import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
@@ -18,7 +19,9 @@ function App() {
           <Header/>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
-                <Route path="/profile" element={<Profile />}></Route>
+                <Route path="/profile" element={<PrivateRoute />}>
+                      <Route path="/profile" element={<Profile />}></Route>
+                </Route>
                 <Route path="/sign-in" element={<SignIn />}></Route>
                 <Route path="/sign-up" element={<SignUp />}></Route> 
                 <Route path="/forgot-password" element={<ForgotPassword />}></Route>  
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { getAuth } from 'firebase/auth';
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Profile() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+      name: auth.currentUser.displayName,
+      email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+  function onLogout() {
+      auth.signOut();
+      navigate("/");
+  }
   return (
     <div>
-        <button type='submit' className='text-center mt-4 bg-blue-200 text-black uppercase px-5 py-2
-        text-sm font-bold rounded shadow-md hover:bg-blue-400 transition duration ease-in-out hover:shadow-lg
-        active:bg-blue-500'>
-            <Link to="/create-listing" className='flex justify-center'>
-                Make New announcement
-            
-            </Link>  
-        </button>
+        <section className="max-w-6xl mx-auto flex flex-col justify-center items-center">
+            <h1 className="text-2xl text-center mt-6 font-bold">My Profile</h1>
+            <div className="w-full md:w-[50%] mt-6 px-3">
+                <form>
+                      { /* name input */ } 
+                      <input 
+                      type='text' 
+                      id='name' 
+                      value={name}
+                      disabled
+                      className="w-[65%] px-4 py-1 text-md text-black bg-white border border-gray-300
+                      rounded transition ease-in-out mb-5"/>
+
+                      { /* email input */ } 
+                      <input 
+                      type='email' 
+                      id='email' 
+                      value={email}
+                      disabled
+                      className="w-[65%] px-4 py-1 text-md text-black bg-white border border-gray-300
+                      rounded transition ease-in-out mb-1"/>
+
+                      <div className="flex whitespace-nowrap text-sm sm:text-md mb-5">
+                          <p className="flex items-center">Do you want to make changes?
+                            <span className="ml-2 text-red-600 hover:text-red-800 transition 
+                            ease-in-out duration-200 cursor-pointer">Edit</span>
+                          </p>
+                      </div>
+                      <button onClick={onLogout} className="bg-blue-600 text-white px-3 py-1 ml-40 text-sm font-medium
+                      rounded shadow-lg hover:bg-blue-800 transition duration-150 ease-in-out 
+                      hover:shadow-lg active:bg-blue-900">Sign Out</button>
+                </form>
+            </div>
+            <button type='submit' className='text-center mt-4 bg-blue-200 text-black uppercase px-5 py-2
+                    text-sm font-bold rounded shadow-md hover:bg-blue-400 transition duration ease-in-out hover:shadow-lg
+                    active:bg-blue-500'>
+                        <Link to="/create-listing" className='flex justify-center'>
+                            Make New announcement
+                        </Link>  
+            </button>
+        </section>
     </div>
   )
 }
